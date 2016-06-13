89a0a1885bfffaa825b12ea8c157265ae5ab0f51
tomer_yeh <hzkel02@gmail.com>
2016-06-13 21:13:05 +0300

    google

--- app/scripts/controllers/loginCtrl.js
+++ app/scripts/controllers/loginCtrl.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('doodleApp')
-  .controller('LoginCtrl', function ($scope,$firebaseObject,) {
-
+  .controller('LoginCtrl', function ($scope,$firebaseAuth) {
+	 var ref = new Firebase("https://project-7335870999437412984.firebaseio.com");
+	  // create an instance of the authentication service
+	  var auth = $firebaseAuth(ref);
+	  // login with Facebook
+	  auth.$authWithOAuthPopup("facebook").then(function(authData) {
+	    console.log("Logged in as:", authData.uid);
+	  }).catch(function(error) {
+	    console.log("Authentication failed:", error);
+	  });
  
 
 
