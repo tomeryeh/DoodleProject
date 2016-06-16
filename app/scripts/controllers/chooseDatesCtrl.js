06ddbd7bdf7feabefd3721212baecc9529d3f591
tomeryeh <hzkel02@gmail.com>
2016-06-16 12:07:22 +0300

    add next + fix choose dates

--- app/scripts/controllers/chooseDatesCtrl.js
+++ app/scripts/controllers/chooseDatesCtrl.js
@@ -1,31 +1,22 @@
 ﻿'use strict';
 
-app.controller('ChooseDatesCtrl', function ($scope, $firebaseObject, $location,Auth) {
-    $scope.auth = Auth;
+app.controller('ChooseDatesCtrl', function ($scope, $firebaseObject, $location) {
 
-    var queryString = $location.search();
-    var eventId = queryString.event;
+    $scope.init = function() {
+        var queryString = $location.search();
+        var eventId = queryString.event;
 
-    var ref = firebase.database().ref();
+        var ref = firebase.database().ref();
 
-    $scope.DBData = $firebaseObject(ref.child('meetings').child(eventId).child('data'));
+        $scope.DBData = $firebaseObject(ref.child('meetings').child(eventId).child('data'));
 
-    $scope.DBData.$loaded().then(function () {
-        $scope.dates = $scope.DBData.dates;
-        
-    });
-
-    $scope.auth.$onAuthStateChanged(function(firebaseUser) {
-      $scope.firebaseUser = firebaseUser;
-
-    if(firebaseUser){
-      $location.path('/result');
+        $scope.DBData.$loaded().then(function () {
+            $scope.dates = $scope.DBData.dates;
+        });
     };
-
-    });
-
+    
     $scope.savePick = function() {
-		var curUserName = $scope.chooseDateEmail;// $scope.firebaseUser.displayName+'|'+$scope.firebaseUser.email;
+		var curUserName = $scope.chooseDateEmail;
     	for (var i = $scope.dates.length - 1; i >= 0; i--) {
     		if ($scope.dates[i].isMark){
     			$scope.dates[i].names = $scope.dates[i].names || [];
@@ -33,12 +24,11 @@ app.controller('ChooseDatesCtrl', function ($scope, $firebaseObject, $location,A
                 $scope.dates[i].names.push(curUserName);
     		}else{
                 $scope.dates[i].names = $scope.dates[i].names || [];
-                // if($scope.dates[i].names.length > 0){
-                //     $scope.dates[i].names =[];
-                // }
             }
     	}
         $scope.DBData.dates = $scope.dates;
         $scope.DBData.$save();
     };
+
+    $scope.init();
 });
